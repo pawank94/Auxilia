25565f2c5bb71e2003b3b76dcaa91d3f5cd450ac
pawank94 <pawanknitkkr@gmail.com>
2016-03-05 21:00:28 +0530

    initial... frequency meter and webkit api integrated

---
+++ src/viz.js
@@ -0,0 +1,34 @@
+var showAxes = function() {
+  document.getElementById('spectrum-axes').style.display = "block";
+  document.getElementById('spectrum-zoom-axes').style.display = "block";
+};
+
+var drawBall = function(ctx, pos) {
+  ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+  ctx.fillStyle = "rgb(0,0,0)";
+
+  var normalizedX = Math.floor( 0.5 * ctx.canvas.width );
+  var normalizedY = Math.floor( 0.5 * ctx.canvas.height );
+  var maxSize = 100;
+  var normalizedSize = maxSize/2 + Math.floor( pos/30 * maxSize );
+  ctx.fillRect( normalizedX - normalizedSize/2,
+                ctx.canvas.height - normalizedY - normalizedSize/2,
+                normalizedSize, normalizedSize );
+};
+
+var drawFrequencies = function(ctx, analyser, freqs, primaryTone, startFreq, endFreq) {
+  ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+  ctx.fillStyle = "rgba(0,0,0,0.8)";
+
+  var primaryVolume = freqs[primaryTone];
+  // For some reason, primaryVolume becomes 0 on firefox, I have no idea why
+  if (primaryVolume == 0) {
+    primaryVolume = 255;
+  }
+  for (var i = 0; i < (endFreq-startFreq); i++) {
+    var volume = freqs[startFreq+i];
+    var normalizedX = Math.floor( i/(endFreq-startFreq) * ctx.canvas.width );
+    var normalizedY = Math.floor( 0.9 * volume/primaryVolume * ctx.canvas.height );
+    ctx.fillRect( normalizedX, ctx.canvas.height - normalizedY - 20, 1, 4 );
+  }
+};
